73420e63dd17f6813077baffed749a074797e0b6
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-05-22 21:29:51 -0300

    Exception Handling using @ControllerAdvice, @ResponseStatus, and @ExceptionHandler

--- 03-calculator/src/main/java/com/levi/MathController.java
+++ 03-calculator/src/main/java/com/levi/MathController.java
@@ -6,22 +6,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MathController {   
+public class MathController {
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupportedOperationException("Please set a numeric value!");
         }
 
         Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
         return sum;
     }
 
-    private Double convertToDouble(String numberOne) {
-        return 1D;
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number))
+            return Double.parseDouble(number);
+        return 0D;
     }
 
-    private boolean isNumeric(String number) {
-        return false;
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
