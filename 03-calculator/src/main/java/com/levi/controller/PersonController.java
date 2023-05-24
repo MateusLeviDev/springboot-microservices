8c2fdaee6685fbb7c1dcbd70ac655be2b0e4faf9
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-05-24 20:08:35 -0300

    retornando listas

--- 03-calculator/src/main/java/com/levi/controller/PersonController.java
+++ 03-calculator/src/main/java/com/levi/controller/PersonController.java
@@ -9,12 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private PersonServices services;
+
+    @RequestMapping(
+            method=RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return services.findAll();
+    }
 
     @RequestMapping(value="/{id}",
             method=RequestMethod.GET,
--- 03-calculator/src/main/java/com/levi/services/PersonServices.java
+++ 03-calculator/src/main/java/com/levi/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.levi.services;
 import com.levi.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 //spring cuidará da injeção de dependência
@@ -21,4 +23,22 @@ public class PersonServices {
         return person;
     }
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name" + i);
+        person.setLastName("Last name" + i);
+        person.setAddress("Some address in Brasil" + i);
+        person.setGender("Gender" + i);
+        return person;
+    }
 }
