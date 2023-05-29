281a992df1d3ccf305bf2bc45191a8a0c468f1a1
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-05-29 20:38:01 -0300

    feat: creating Greeting Configuration [Spring Actuator]

--- 04-greeting-service/src/main/java/com/levi/configuration/GreetingConfiguration.java
+++ 04-greeting-service/src/main/java/com/levi/configuration/GreetingConfiguration.java
@@ -1,2 +1,31 @@
-package com.levi.configuration;public class GreetingConfiguration {
+package com.levi.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greeting-service") //faz o building entre o arq de config com a classe java
+public class GreetingConfiguration {
+
+    private String greeting;
+    private String defaultValue;
+
+    public GreetingConfiguration() {
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
 }
