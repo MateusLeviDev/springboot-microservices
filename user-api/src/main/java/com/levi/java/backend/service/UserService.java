d7b79b6be3d9de6340f5318f66b3f60bad3c50a2
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-06 23:49:10 -0300

    `feat(service)`: implement user service layer

--- user-api/src/main/java/com/levi/java/backend/service/UserService.java
+++ user-api/src/main/java/com/levi/java/backend/service/UserService.java
@@ -3,7 +3,9 @@ package com.levi.java.backend.service;
 import com.levi.java.backend.domain.User;
 import com.levi.java.backend.dto.UserDTO;
 import com.levi.java.backend.repository.UserRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,22 +13,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository repository;
 
     public List<UserDTO> getAll() {
         List<User> users = repository.findAll();
-
         return users.stream()
                 .map(UserDTO::convert)
                 .collect(Collectors.toList());
     }
 
-    public UserDTO findById(long userId) {
-        User user = repository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
+    public UserDTO findById(Long userId) {
+        User user = repository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
         return UserDTO.convert(user);
     }
 
@@ -36,29 +36,28 @@ public class UserService {
         return UserDTO.convert(savedUser);
     }
 
-    public UserDTO delete(long userId) {
+    public void delete(Long userId) {
         User user = repository.findById(userId).orElseThrow(RuntimeException::new);
         repository.delete(user);
-        return UserDTO.convert(user);
     }
 
     public UserDTO findByCpf(String cpf) {
-        User byCpf = repository.findByCpf(cpf);
-        if (byCpf != null) {
-            return UserDTO.convert(byCpf);
+        User user = repository.findByCpf(cpf);
+        if (user != null) {
+            return UserDTO.convert(user);
         }
+
         return null;
     }
 
     public List<UserDTO> queryByName(String name) {
         List<User> users = repository.queryByNameLike(name);
-        return users
-                .stream()
+        return users.stream()
                 .map(UserDTO::convert)
                 .collect(Collectors.toList());
     }
 
-    public UserDTO editUser(Long userId, UserDTO userDTO) {
+    public UserDTO replace(Long userId, UserDTO userDTO) {
         User user = repository
                 .findById(userId).orElseThrow(RuntimeException::new);
         if (userDTO.getEmail() != null &&
@@ -76,4 +75,10 @@ public class UserService {
         user = repository.save(user);
         return UserDTO.convert(user);
     }
+
+    public Page<UserDTO> getAllPageable(Pageable page) {
+        Page<User> users = repository.findAll(page);
+        return users.map(UserDTO::convert);
+    }
+
 }
