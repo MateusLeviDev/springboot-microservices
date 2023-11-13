76b33f605cddff37048603fcf34de702d92b3859
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-13 08:04:50 -0300

    `chore`: concluding controller endpoints

--- user-api/src/main/java/com/levi/java/backend/controller/UserController.java
+++ user-api/src/main/java/com/levi/java/backend/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.levi.java.backend.controller;
 
 import com.levi.java.backend.domain.User;
-import com.levi.java.backend.requests.UserPostRequest;
-import com.levi.java.backend.service.UserService;
+import com.levi.java.backend.mapper.requests.UserPostRequest;
+import com.levi.java.backend.mapper.requests.UserPutRequest;
+import com.levi.java.backend.mapper.response.UserResponse;
+import com.levi.java.backend.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,52 +22,52 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService service;
+    private final UserServiceImpl service;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
     }
 
-//    @GetMapping()
-//    public ResponseEntity<List<UserCreateDTO>> getAll() {
-//        return ResponseEntity.ok(service.getAll());
-//    }
-//
-//    @GetMapping("/{userId}")
-//    public ResponseEntity<UserCreateDTO> findById(@PathVariable Long userId) {
-//        return ResponseEntity.ok(service.findById(userId));
-//    }
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> findById(@PathVariable long userId) {
+        return ResponseEntity.ok(service.findByIdOrThrowBadRequestException(userId));
+    }
+
+    @GetMapping("/pageable")
+    public ResponseEntity<Page<User>> getAllPageable(@PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 15) Pageable pageable) {
+        return ResponseEntity.ok(service.findAllPageable(pageable));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<UserResponse> findByCpf(@PathVariable String cpf) {
+        return ResponseEntity.ok(service.findByCpf(cpf));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<UserPostRequest>> findByName(@RequestParam(name = "name") String name) {
+        return ResponseEntity.ok(service.queryByName(name));
+    }
 
     @PostMapping
     public ResponseEntity<User> save(@RequestBody @Valid UserPostRequest userPostRequest) {
         return new ResponseEntity<>(service.save(userPostRequest), HttpStatus.CREATED);
     }
-//
-//    @DeleteMapping
-//    public ResponseEntity<Void> delete(@PathVariable long userId) {
-//        service.delete(userId);
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
-//
-//    @GetMapping("/{cpf}/cpf")
-//    public ResponseEntity<UserCreateDTO> findByCpf(@PathVariable String cpf) {
-//        return ResponseEntity.ok(service.findByCpf(cpf));
-//    }
-//
-//    @GetMapping("/search")
-//    public List<UserCreateDTO> queryByName(@RequestParam(name = "nome", required = true) String nome) {
-//        return service.queryByName(nome);
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<UserCreateDTO> replace(@PathVariable long userId, @RequestBody UserCreateDTO userDTO) {
-//        service.replace(userId, userDTO);
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
-//
-//    @GetMapping("/pageable")
-//    public Page<UserCreateDTO> getUsersPage(Pageable pageable) {
-//        return service.getAllPageable(pageable);
-//    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> delete(@PathVariable long userId) {
+        service.delete(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> replace(@RequestBody UserPutRequest userPutRequest) {
+        service.replace(userPutRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
