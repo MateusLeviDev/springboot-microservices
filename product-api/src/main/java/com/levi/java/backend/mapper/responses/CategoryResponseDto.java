472ebb2834117b72d8d909cc8ab0afc7858b1682
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-12-13 14:57:20 -0300

    `chore`: removing and renaming classes

--- product-api/src/main/java/com/levi/java/backend/mapper/responses/CategoryResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.levi.java.backend.mapper.responses;
-
-import com.levi.java.backend.mapper.util.Category;
-
-public class CategoryResponseDto extends Category {
-
-}
--- product-api/src/main/java/com/levi/java/backend/mapper/responses/ProductResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.levi.java.backend.mapper.responses;
-
-import com.levi.java.backend.mapper.util.Product;
-
-public class ProductResponseDto extends Product {
-
-}
