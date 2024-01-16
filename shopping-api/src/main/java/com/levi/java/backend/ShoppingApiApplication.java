383f32bc5f0ab286ba3964c3b4670b1186adfc95
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-01-15 22:53:03 -0300

    Initial commit

---
+++ shopping-api/src/main/java/com/levi/java/backend/ShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package com.levi.java.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApiApplication.class, args);
+	}
+
+}
