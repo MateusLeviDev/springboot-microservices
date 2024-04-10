4c65959ec61a2e4898a77648317886f1d5e0331f
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-04-10 19:22:58 -0300

    removing old content

--- product-api/src/main/java/com/levi/java/backend/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.levi.java.backend.repository;
-
-import com.levi.java.backend.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
