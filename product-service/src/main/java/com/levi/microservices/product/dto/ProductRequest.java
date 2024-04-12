ca2627ecd4c89c8996b1b568310c36069746a4d5
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-04-11 23:37:54 -0300

    `chore`: removing unused files and removing id attribute from dto

--- product-service/src/main/java/com/levi/microservices/product/dto/ProductRequest.java
+++ product-service/src/main/java/com/levi/microservices/product/dto/ProductRequest.java
@@ -3,7 +3,6 @@ package com.levi.microservices.product.dto;
 import java.math.BigDecimal;
 
 public record ProductRequest(
-        String id,
         String name,
         String description,
         BigDecimal price
