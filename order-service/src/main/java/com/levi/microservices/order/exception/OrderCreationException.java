4c5455b152f03fbce316edb39176711650a3ec8f
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-04-20 22:05:09 -0300

    `feat`: impl communication between services. Integrate synchronous connection with OpenFeign

---
+++ order-service/src/main/java/com/levi/microservices/order/exception/OrderCreationException.java
@@ -0,0 +1,17 @@
+package com.levi.microservices.order.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponseException;
+
+public class OrderCreationException extends ErrorResponseException {
+    public OrderCreationException(final String message) {
+        super(HttpStatus.INTERNAL_SERVER_ERROR, asProblemDetail(message), null);
+    }
+
+    private static ProblemDetail asProblemDetail(final String message) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, message);
+        problemDetail.setTitle("Order creation failed");
+        return problemDetail;
+    }
+}
