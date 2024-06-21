e56bccf56bb9a98db5bd963427991ecd5043ddba
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-06-20 23:20:52 -0300

    `test`: integration tests for PUT, GET and DELETE methods. TODO more cases and exception handler

--- product-service/src/test/java/com/levi/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/levi/microservices/product/ProductServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.levi.microservices.product;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,18 +19,13 @@ class ProductServiceApplicationTests {
     @LocalServerPort
     private Integer port;
 
+    private String productId;
+
     @BeforeEach
     void setup() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
-    }
 
-    static {
-        mongoDBContainer.start();
-    }
-
-    @Test
-    void contextLoads() {
         String requestBody = """
                 {
                 	"name": "RTX4060",
@@ -38,8 +34,8 @@ class ProductServiceApplicationTests {
                 }
                 """;
 
-        RestAssured.given()
-                .contentType("application/json")
+        productId = RestAssured.given()
+                .contentType(ContentType.JSON)
                 .body(requestBody)
                 .when()
                 .post("/api/product")
@@ -48,7 +44,116 @@ class ProductServiceApplicationTests {
                 .body("id", Matchers.notNullValue())
                 .body("name", Matchers.equalTo("RTX4060"))
                 .body("description", Matchers.equalTo("GPU"))
+                .body("price", Matchers.equalTo(2000))
+                .extract().path("id");
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                	"name": "RTX4080",
+                	"description": "GPU",
+                	"price": 3000
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("RTX4080"))
+                .body("description", Matchers.equalTo("GPU"))
+                .body("price", Matchers.equalTo(3000));
+    }
+
+    @Test
+    void shouldGetAllProducts() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/product/" + productId)
+                .then()
+                .statusCode(200)
+                .body("id", Matchers.equalTo(productId))
+                .body("name", Matchers.equalTo("RTX4060"))
+                .body("description", Matchers.equalTo("GPU"))
                 .body("price", Matchers.equalTo(2000));
+    }
+
+    @Test
+    void shouldUpdateProduct() {
+        String updateRequestBody = """
+                {
+                	"name": "RTX4070",
+                	"description": "Updated GPU",
+                	"price": 2500
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(updateRequestBody)
+                .when()
+                .put("/api/product/" + productId)
+                .then()
+                .statusCode(204);
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/product/" + productId)
+                .then()
+                .statusCode(200)
+                .body("id", Matchers.equalTo(productId))
+                .body("name", Matchers.equalTo("RTX4070"))
+                .body("description", Matchers.equalTo("Updated GPU"))
+                .body("price", Matchers.equalTo(2500));
+    }
+
+    @Test
+    void shouldDeleteProduct() {
+        // Create a new product to delete
+        String requestBody = """
+                {
+                	"name": "RTX4090",
+                	"description": "GPU",
+                	"price": 3500
+                }
+                """;
+
+        String deleteProductId = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        // Delete the product
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/api/product/" + deleteProductId)
+                .then()
+                .statusCode(204);
+
+        // Verify deletion
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/product/" + deleteProductId)
+                .then()
+                .statusCode(500); //TODO: global exception handler
     }
 
 }
